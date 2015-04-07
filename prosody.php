26a4266494b74ca591602e4fdc62016ebbe1c363
Scott Bailey <csb5t@virginia.edu>
2015-04-07 14:11:21 -0400

    Starts sketching out ideas of how to do this server side

--- prosody.php
+++ prosody.php
@@ -29,9 +29,52 @@ function prosody_create_post_type ()
     );
 }
 
+// Will using the save action actually be a problem due to autosaving in WP?
 add_action( 'save_post_prosody_poem', 'prosody_xml_transform');
 
 function prosody_xml_transform ($post)
 {
+    global $post;
+
+    $tei = $post->post_content;
+
+    // Add check to see if there is post meta on field, if so update post meta, otherwise add post meta
+
+    add_post_meta( $post, 'key', $tei );
+
+    $original = get_post_meta( $post, 'key' );
+    echo $original;
+
+
+    // $post_url = post_permalink( $post );
+    // wp_redirect( $post_url ); exit;
 
 }
+
+// Process:
+// Option 1:
+//      Create custom post type for Poem
+//      Put TEI into Post content field
+//      On Save action for Poem type:
+//          Create XSLT Process, read in post content as xml file (can this be done?),
+//          Read in XSL file
+//          Transform to html
+//          Attach post content (TEI) as post meta data (will need to define meta field)
+//          Replace post content with generated html
+//          Resave the post (For the second save, will need to check if html or tei - if tei, run xslt, if html, don't)
+
+// Option 2:
+//  Enable file upload with xml and xsl types for custom post type
+//  Use XSLT Process, load in both files and transform
+//  Set post content equal to generated html on save? Or is there an action that could be fired as soon the two files are uploade?
+
+// Option 3:
+// Instead of working on the server, just input TEI into post content field
+// Use Saxon CE to transform in the client
+
+
+
+
+
+
+

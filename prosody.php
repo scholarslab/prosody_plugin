8015d3104502295eca40eab6072bb6e78700636a
Scott Bailey <csb5t@virginia.edu>
2015-04-08 09:02:35 -0400

    Transforms XML in meta field to HTML in post_content
    - Implements metabox for original document for poems
    - On save, adds post meta containing XML put into metabox
    - On save, uses XSLT to transform XML from meta into HTML
    - On save, sets post_content equal to generated HTML

--- prosody.php
+++ prosody.php
@@ -36,7 +36,7 @@ function prosody_xml_transform ($post)
 {
     global $post;
 
-    $tei = $post->post_content;
+    $tei = get_post_meta( $post->ID, 'Original Document', true );
 
     $xml_doc = new DOMDocument();
     $xml_doc->loadXML( $tei );
@@ -47,8 +47,18 @@ function prosody_xml_transform ($post)
     $proc = new XSLTProcessor();
     $proc->importStylesheet( $xsl_doc );
     $newdom = $proc->transformToDoc( $xml_doc );
+    // $newhtml = $newdom->saveHTML();
 
-    print $newdom->saveXML();
+    $my_post = array(
+        'ID' => $post->ID,
+        'post_content' => $newdom->saveXML(),
+    );
+
+    wp_update_post( $my_post, true );
+
+
+    // print $newdom->saveXML();
+    // print $tei;
 
     // $post->post_content = $newdom->saveXML();
 
@@ -58,6 +68,109 @@ function prosody_xml_transform ($post)
 
     // Consider adding initial xml into post meta field, then transforming and filling in post content
 
+}
+
+// Adds meta box
+add_action( 'add_meta_boxes', 'prosody_source_poem' );
+
+function prosody_source_poem ()
+{
+    add_meta_box(
+        'prosody_source_poem',
+        __('Original Document:', 'prosody'),
+        'prosody_source_poem_meta_box',
+        'prosody_poem'
+    );
+}
+
+// Prints meta box
+function prosody_source_poem_meta_box($post=null)
+{
+    $post = (is_null($post)) ? get_post() : $post;
+
+    //Add a nonce field so we can check for it later
+    wp_nonce_field(
+        'prosody_source_poem_meta_box',
+        'prosody_source_poem_meta_box_nonce'
+    );
+
+    /*
+     * Use get_post_meta() to retrieve an existing value
+     * from the database and use the value for the form.
+     */
+    $value = get_post_meta( $post->ID, 'Original Document', true);
+
+    echo '<label for="prosody_source_poem">';
+    __( 'Original Document:', 'prosody' );
+    echo '</label> ';
+    echo '<input type="text" id="prosody_source_poem" '
+    . 'name="prosody_source_poem" value="' . esc_attr( $value ) .
+    '" size="100" />';
+}
+
+
+// Saves meta data
+add_action( 'save_post_prosody_poem', 'prosody_source_poem_save_meta_box_data');
+/**
+ * When the post is saved, saves our custom data.
+ *
+ * @param int $post_id The ID of the post being saved.
+ */
+function prosody_source_poem_save_meta_box_data ($post_id=null)
+{
+    $post_id = (is_null($post_id)) ? get_post()->ID : $post_id;
+
+    /*
+     * We need to verify this came from our screen and with proper
+     * authorization, because the save_post action can be triggered at other
+     * times.
+     */
+
+    // Check if our nonce is set.
+    if ( ! isset( $_POST['prosody_source_poem_meta_box_nonce'] ) ) {
+        return;
+    }
+
+    // Verify that the nonce is valid.
+    $nonce = $_POST['prosody_source_poem_meta_box_nonce'];
+    if ( ! wp_verify_nonce( $nonce, 'prosody_source_poem_meta_box' ) ) {
+        return;
+    }
+
+    // If this is an autosave, our form has not been submitted, so we don't
+    // want to do anything.
+    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+        return;
+    }
+
+    // Check the user's permissions.
+    if ( isset( $_POST['post_type'] ) &&
+       'prosody_poem' == $_POST['post_type'] ) {
+
+        if ( ! current_user_can( 'edit_page', $post_id ) ) {
+            return;
+        }
+
+    } else {
+
+        if ( ! current_user_can( 'edit_post', $post_id ) ) {
+            return;
+        }
+    }
+
+    /* OK, its safe for us to save the data now. */
+
+    // Make sure that it is set.
+    if ( ! isset( $_POST['prosody_source_poem'] ) ) {
+        return;
+    }
+
+    // Sanitize user input.
+    // $my_data = sanitize_text_field( $_POST['prosody_source_poem'] );
+    $my_data = $_POST['prosody_source_poem'];
+
+    // Update the meta field in the database.
+    update_post_meta( $post_id, 'Original Document', $my_data );
 }
 
 // Process:

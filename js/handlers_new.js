666ed9de4d5688b23ba906f2a8df76037618a6ef
Scott Bailey <csb5t@virginia.edu>
2015-06-15 15:37:37 -0400

    Outline rewrite of JS away from Prototype/Mootools/etc..

---
+++ js/handlers_new.js
@@ -0,0 +1,100 @@
+// symbols used;
+var space = '\u00A0';
+var cup = '\u222a';
+var backslash = '';
+
+function removeAllChildren (argument) {
+    // Will need to rewrite without prototype elements
+}
+
+function getInnerText (argument) {
+    // body...
+}
+
+function pullText (argument) {
+    // what's the difference between this and getInnerText
+}
+
+function init (argument) {
+    // this does a lot. Switch to using jquery on event
+}
+
+function checkRhyme (argument) {
+    // Checks whether the user entered rhyme is correct
+}
+
+function switchStress (argument) {
+    // Changes the stress
+}
+
+function checkStress (argument) {
+    // check the user entered stress
+}
+
+function switchFoot (argument) {
+    // switch the foot
+}
+
+function checkFeet (argument) {
+    // body...
+}
+
+function checkMeter (argument) {
+    // body...
+}
+
+function grabText (argument) {
+    // what is the difference between this and getInnerText/pullText
+}
+
+function addMarker (argument) {
+    // body...
+}
+
+function marker (argument) {
+    // not sure what this does.
+}
+
+function slackMarker (argument) {
+    // body...
+}
+
+function footMarker (argument) {
+    // body...
+}
+
+function placeholder (argument) {
+    // ?
+}
+
+function toggleStress (argument) {
+    // body...
+}
+
+function toggleFeet (argument) {
+    // body...
+}
+
+function toggleCaesura (argument) {
+    // body...
+}
+
+function toggleDifferences (argument) {
+    // body...
+}
+
+function updateHintButton (argument) {
+    // probably not necessary since hints aren't currently showing up.
+}
+
+function popHint (argument) {
+    // also probably no longer necessary
+}
+
+function clickableHintImage (argument) {
+    // also probably no longer necessary
+}
+
+function debug (argument) {
+    // necessary?
+}
---
+++ js/todo.md
@@ -0,0 +1,20 @@
+- Need to be able to manipulate stress markers, feet
+
+    - collection of symbols/characters for each variation
+    - function for each of four markers such that on click, the app rotates through the markers for that type
+    - function to highlight where markers can be placed for ui
+
+- check meter
+    - create popup with two dropdown elements - meter, number of feet
+
+- check stress
+
+- check feet
+    - check feet that are currently in the shadow l
+
+- check * buttons ()
+    - for each of three buttons (check stress, check feet, check meter), display small popup on hover that says what button does
+    - on click, runs function actually check correctness. If incorrect, shows red x icon, if correct, shows ... (probably green check mark)
+
+- switch foot
+    - function such that clicking a .prosody-syllable creates a pipe after the syllable - a span.prosody-footmarker

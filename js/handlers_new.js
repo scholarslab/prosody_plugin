4f27831e573aa9adde34715d628669742b40a070
Scott Bailey <csb5t@virginia.edu>
2015-06-16 09:38:18 -0400

    low caps to fit existing markup

--- js/handlers_new.js
+++ js/handlers_new.js
@@ -3,98 +3,101 @@ var space = '\u00A0';
 var cup = '\u222a';
 var backslash = '';
 
-function removeAllChildren (argument) {
-    // Will need to rewrite without prototype elements
-}
+// function removeAllChildren (argument) {
+//     // Will need to rewrite without prototype elements
+// }
 
-function getInnerText (argument) {
-    // body...
-}
+// function getInnerText (argument) {
+//     // body...
+// }
 
-function pullText (argument) {
-    // what's the difference between this and getInnerText
-}
+// function pullText (argument) {
+//     // what's the difference between this and getInnerText
+// }
 
-function init (argument) {
-    // this does a lot. Switch to using jquery on event
-}
+// function init (argument) {
+//     // this does a lot. Switch to using jquery on event
+// }
 
-function checkRhyme (argument) {
+function checkrhyme (argument) {
     // Checks whether the user entered rhyme is correct
 }
 
-function switchStress (argument) {
-    // Changes the stress
-}
-
-function checkStress (argument) {
+function checkstress (argument) {
     // check the user entered stress
 }
 
-function switchFoot (argument) {
+function checkfeet (argument) {
+    // body...
+}
+
+function checkmeter (argument) {
+    // body...
+}
+
+function switchstress (argument) {
+    // Changes the stress
+}
+
+
+function switchfoot (argument) {
     // switch the foot
 }
 
-function checkFeet (argument) {
+
+function togglestress (argument) {
     // body...
 }
 
-function checkMeter (argument) {
+function togglefeet (argument) {
     // body...
 }
 
-function grabText (argument) {
-    // what is the difference between this and getInnerText/pullText
-}
-
-function addMarker (argument) {
+function togglecaesura (argument) {
     // body...
 }
 
-function marker (argument) {
-    // not sure what this does.
-}
-
-function slackMarker (argument) {
+function toggledifferences (argument) {
     // body...
 }
 
-function footMarker (argument) {
-    // body...
-}
+// function grabText (argument) {
+//     // what is the difference between this and getInnerText/pullText
+// }
 
-function placeholder (argument) {
-    // ?
-}
+// function addMarker (argument) {
+//     // body...
+// }
 
-function toggleStress (argument) {
-    // body...
-}
+// function marker (argument) {
+//     // not sure what this does.
+// }
 
-function toggleFeet (argument) {
-    // body...
-}
+// function slackMarker (argument) {
+//     // body...
+// }
 
-function toggleCaesura (argument) {
-    // body...
-}
+// function footMarker (argument) {
+//     // body...
+// }
 
-function toggleDifferences (argument) {
-    // body...
-}
+// function placeholder (argument) {
+//     // ?
+// }
 
-function updateHintButton (argument) {
-    // probably not necessary since hints aren't currently showing up.
-}
 
-function popHint (argument) {
-    // also probably no longer necessary
-}
+// function updateHintButton (argument) {
+//     // probably not necessary since hints aren't currently showing up.
+// }
 
-function clickableHintImage (argument) {
-    // also probably no longer necessary
-}
+// function popHint (argument) {
+//     // also probably no longer necessary
+// }
 
-function debug (argument) {
-    // necessary?
-}
+// function clickableHintImage (argument) {
+//     // also probably no longer necessary
+// }
+
+// function debug (argument) {
+//     // necessary?
+// }
